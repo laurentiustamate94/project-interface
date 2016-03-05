8a0fdbc06e771df60d2445a7d8c95f2762e47dbd
laurentiustamate94 <laurentiustamate94@yahoo.com>
2016-03-06 01:03:52 +0200

    Vision API - created ProjectOxford partial class to better implement and maintain the various APIs - created ProjectOxford.Vision used for exposing the image analizer and object character recognition APIs

---
+++ Interface/Interface/ImageProcessing/ProjectOxford.cs
@@ -0,0 +1,15 @@
+﻿using Interface.Interfaces;
+using Microsoft.ProjectOxford.Vision;
+
+namespace Interface.ImageProcessing
+{
+    public partial class ProjectOxford : IImageProcessing
+    {
+        private readonly IVisionServiceClient visionClient;
+
+        public ProjectOxford()
+        {
+            visionClient = new VisionServiceClient(VISION_API_KEY);
+        }
+    }
+}
