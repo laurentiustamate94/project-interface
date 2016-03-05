8fc635a7c99066a86dd586ca006670f91847da17
laurentiustamate94 <laurentiustamate94@yahoo.com>
2016-03-06 00:35:07 +0200

    Image Source - added IImageSource interface to specify an abstract way to handle image sources - implemented IImageSource to make use of the local camera attached to the device

---
+++ Interface/Interface/ImageSource/LocalCameraImageSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Interface.Interfaces;
+using Windows.Media.Capture;
+
+namespace Interface.ImageSource
+{
+    public class LocalCameraImageSource : IImageSource
+    {
+        public IPhotoStorage Storage { get; set; }
+
+        public MediaCapture MediaDevice { get; private set; }
+
+        public LocalCameraImageSource(IPhotoStorage storage, MediaCapture mediaDevice)
+        {
+            Storage = storage;
+            MediaDevice = mediaDevice;
+        }
+
+        public async Task InitializeDevice()
+        {
+            await MediaDevice.InitializeAsync();
+
+            MediaDevice.Failed += new MediaCaptureFailedEventHandler(MediaCapture_Failed);
+        }
+
+        public void TakePhoto(string filename)
+        {
+            Storage.Save(filename);
+        }
+
+        private async void MediaCapture_Failed(MediaCapture currentCaptureObject, MediaCaptureFailedEventArgs currentFailure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interface/Interface/Interfaces/IImageSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Interface.Interfaces
+{
+    public interface IImageSource
+    {
+        IPhotoStorage Storage { get; set; }
+
+        Task InitializeDevice();
+
+        void TakePhoto(string filename);
+    }
+}
