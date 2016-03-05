1af302823204665a5a1d7a33240e5d6449ba2af4
laurentiustamate94 <laurentiustamate94@yahoo.com>
2016-03-06 00:34:21 +0200

    Photo Storage - added IPhotoStorage interface to specify an abstract way to handle image storage - implemented IPhotoStorage to make use of the local storage of the device

---
+++ Interface/Interface/Interfaces/IPhotoStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Interface.Interfaces
+{
+    public interface IPhotoStorage
+    {
+        Task Save(string filename);
+
+        Task<BitmapImage> GetPhoto(string filename);
+    }
+}
---
+++ Interface/Interface/Storage/LocalPhotoStorage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Interface.Interfaces;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage;
+using Windows.Media.MediaProperties;
+using Windows.Media.Capture;
+
+namespace Interface.Storage
+{
+    public class LocalPhotoStorage : IPhotoStorage
+    {
+        public StorageFile ImageFile { get; set; }
+
+        public MediaCapture MediaDevice { get; set; }
+
+        public string LastPhotoSavedPath { get; private set; }
+
+        public LocalPhotoStorage(MediaCapture mediaCapture)
+        {
+            MediaDevice = mediaCapture;
+        }
+
+        public async Task<BitmapImage> GetPhoto(string filename)
+        {
+            var image = new BitmapImage();
+            var photoStream = await ImageFile.OpenReadAsync();
+
+            image.SetSource(photoStream);
+
+            return image;
+        }
+
+        public async Task Save(string filename)
+        {
+            var imageProperties = ImageEncodingProperties.CreateJpeg();
+
+            ImageFile = await KnownFolders.PicturesLibrary.CreateFileAsync(
+                    filename, CreationCollisionOption.ReplaceExisting);
+
+            await MediaDevice.CapturePhotoToStorageFileAsync(imageProperties, ImageFile);
+
+            LastPhotoSavedPath = ImageFile.Path;
+        }
+    }
+}
