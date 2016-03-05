d653b75eeb4de22c582e9595d2d827059069a1eb
laurentiustamate94 <laurentiustamate94@yahoo.com>
2016-03-06 00:37:12 +0200

    Image Processing - added IImageProcessing interface to specify an abstract way to handle image processing

---
+++ Interface/Interface/Interfaces/IImageProcessing.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Interface.Interfaces
+{
+    public interface IImageProcessing
+    {
+        Task<object> AnalyzeImage(string imagePathOrUrl);
+
+        Task<string> RecognizeText(string imagePathOrUrl);
+    }
+}
